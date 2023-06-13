953fb59889ee51cb6da7aa1e77e6a7c8ccefe95c
daragiu <daragiualex@yahoo.com>
2023-06-13 12:46:28 +0300

    final

--- figo_project/script.js
+++ figo_project/script.js
@@ -6,7 +6,7 @@ btn.addEventListener('click', function handleClick() {
 
 });
 
-
+//sunet figo
 const audio = new Audio("https://us-tuna-sounds-files.voicemod.net/7b650266-dfec-42f5-b08c-5feae41ef092-1659920498108.mp3");
 const buttons = document.querySelectorAll("button");
 
@@ -15,6 +15,15 @@ buttons.forEach(button => {
     audio.play();
   });
 });
+
+audio.addEventListener("ended", function(){
+  alert("Vrei sa joci din nou?");
+  window.location.reload()
+});
+
+
+
+
 
 // https://us-tuna-sounds-files.voicemod.net/530537b1-41ba-42e0-a8d5-3069a8679a4e-1669093508178.mp3
 // https://orangefreesounds.com/wp-content/uploads/2023/04/Cat-voice-sound.mp3?_=1
